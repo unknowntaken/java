3b0f180afa4621484ff0922484d19e07bd55cd4f
unknowntaken/ <ngokethan@gmail.com>
2017-07-10 10:42:37 -0700

    variable

---
+++ AboutMe.java/src/aboutme/java/AboutMeJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aboutme.java;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class AboutMeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("my name is Ethan Ngo");
+       System.out.println("my age is 14");
+       System.out.println("and my favorite color is green");
+       
+    }
+    
+}
---
+++ variables/src/variables/Variables.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int age=14;
+        double height=5.9;
+        String firstName="Ethan";
+        String lastName="Ngo";
+        boolean isAStudent=true;
+        System.out.println("My name is "+firstName+" "+lastName);
+        
+    }
+    
+}
